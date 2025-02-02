0d9eda9c01f1cfb2c6ee49bcb1c7bbe95f8334ec
Matvey <matvey@Galaxy-J7.localdomain>
2025-02-02 11:41:28 +0100

    scripts dir

---
+++ all-python-scripts/only-check-ports.py
@@ -0,0 +1,46 @@
+import socket
+import requests
+
+
+HOSTS = ["192.168.1.32", "20.103.44.21"]
+
+PORTS = [80, 443, 8080,]
+
+"""Checking the availability of Http/Https ports"""
+def check_http_https(host, port):
+    url = f"http://{host}:{port}" if port == 80 else f"https://{host}:{port}"
+    try:
+        response = requests.get(url, timeout=10, verify=False)  # verify is ignor ssl
+
+        if response.status_code == 200:
+            print(f" [Ok] {url} available")
+            return True
+        else:
+            print(f"[Error] {url} status code {response.status_code}")
+            return False
+        
+    except requests.exceptions.RequestException as e:
+        print(f"[Error] Connection error {url}: {e}")
+        return False
+
+
+'''Check TCP port'''
+def check_tcp_port(host, port):
+    try:
+        with socket.create_connection((host, port), timeout=5):
+            print(f"[Ok] TCP-port {host}:{port} available")
+            return True
+        
+    except socket.error as e:
+        print(f" [Error] Connection error TCP {port} {host} {e}")
+        return False
+
+
+for host in HOSTS:
+    print(f"Check  {host}")
+
+    for port in PORTS:
+        if port in [80, 443]:
+            check_http_https(host, port)
+        else:
+            check_tcp_port(host, port)
